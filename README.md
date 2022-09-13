af486739ebf820c78af6b8daf1c875791acf3fb0
mizaeldouglas <mizaeldouglas2@gmail.com>
2022-09-13 17:02:19 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# nlw-eSposts-react-native-react.js
---
+++ backend/.history/src/server_20220911224401.mjs
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+
+app.get("/ads", (request,response) => {
+  return response.json([
+	  {id: 1, name: "Anuncio 1"},
+	  {id: 1, name: "Anuncio 1"},
+	  {id: 1, name: "Anuncio 1"}
+	  
+  ]);
+})
+
+app.listen(3333)
